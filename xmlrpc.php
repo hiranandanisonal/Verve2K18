07e8c2907dd14582ada156f6ffaa4fb35e3304b9
Abhi241296 <36002969+Abhi241296@users.noreply.github.com>
2018-01-31 20:49:02 +0530

    first commit
    Uploading Files

---
+++ xmlrpc.php
@@ -0,0 +1,14 @@
+<rsd xmlns="http://archipelago.phrasewise.com/rsd" version="1.0">
+<service>
+<engineName>WordPress</engineName>
+<engineLink>https://wordpress.org/</engineLink>
+<homePageLink>http://www.unmaad.com</homePageLink>
+<apis>
+<api name="WordPress" blogID="1" preferred="true" apiLink="http://139.59.69.58/xmlrpc.php"/>
+<api name="Movable Type" blogID="1" preferred="false" apiLink="http://139.59.69.58/xmlrpc.php"/>
+<api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://139.59.69.58/xmlrpc.php"/>
+<api name="Blogger" blogID="1" preferred="false" apiLink="http://139.59.69.58/xmlrpc.php"/>
+<api name="WP-API" blogID="1" preferred="false" apiLink="http://www.unmaad.com/wp-json/"/>
+</apis>
+</service>
+</rsd>
